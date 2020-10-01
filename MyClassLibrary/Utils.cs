6fe0d55cc1dc95f211f3fc5a41243d5bfb3ae111
Aynamik4 <violintailgut@gmail.com>
2020-10-01 11:01:11 +0200

    Initial commit

---
+++ MyClassLibrary/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyClassLibrary
+{
+    public class Utils
+    {
+        static Random random = new Random();
+
+        /// <summary>
+        /// Returns true with a probability of the percent parameter.
+        /// </summary>
+        /// <param name="percent">The probability value.</param>
+        /// <returns></returns>
+        public static bool ProbabilityTrue(int percent)
+        {
+            int randomNumber = random.Next(1, 100 + 1);
+            return randomNumber <= percent;
+        }
+    }
+}
---
+++ MyGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MyClassLibrary;
+
+namespace MyGame
+{
+    class Program
+    {
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                bool x = Utils.ProbabilityTrue(10);
+            }
+        }
+    }
+}
